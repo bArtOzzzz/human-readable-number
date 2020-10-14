7b14400a6337a41b2051e57db4db488f6cf50119
bArtOzzzz <breakyourbrain55@gmail.com>
2020-10-14 19:02:22 +0300

    fix: fix functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-var a = ['zero ','one ','two ','three ','four ', 'five ','six ','seven ','eight ','nine ','ten ','eleven ','twelve ','thirteen ','fourteen ','fifteen ','sixteen ','seventeen ','eighteen ','nineteen '];
-var b = ['', '', 'twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
-
 module.exports = function toReadable (number) {
-  if ((number = number.toString()).length > 3) return 'overflow';
-    n = ('000000000' + number).substr(-9).match(/^(\d{2})(\d{2})(\d{2})(\d{1})(\d{2})$/);
-    if (!n) return; var str = '';
-    str += (n[0] == 0) ? (a[Number(n[0])] || b[n[0][0]] + ' ' + a[n[0][0]]) + '' : '';
-    str += (n[4] != 0) ? (a[Number(n[4])] || b[n[4][0]] + ' ' + a[n[4][1]]) + 'hundred ' : '';
-    str += (n[5] != 0) ? (a[Number(n[5])] || b[n[5][0]] + ' ' + a[n[5][1]]) : '';
-    return str;
-}
+  if (!number || number == 0) return 'zero';
+  let result = '', dig = 0;
+  const digits = ['','one','two','three','four','five','six','seven','eight','nine','ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen','twenty'];
+  if ( number >= 100 ) { 
+      dig = (number/100)|0;
+      result = `${ digits[dig] } hundred`; 
+      number -= dig * 100; 
+      }
+  if ( number > 19  ) {
+	const tens = ['','ten','twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
+      dig = (number/10)|0;
+			number -= dig * 10;
+      result = `${result} ${tens[dig]} ${digits[number]}`;      
+      } 
+			else result = `${result} ${digits[number]}`; 
+  return result.trim();
+} 
